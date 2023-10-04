891799916883c3029c0e2b9c0de3f904ec2dd2e9
Axa Pickering <axapickering@gmail.com>
2023-10-03 17:21:43 -0700

    2nd code review, bernies bikes done

--- sql-ddl-design/sql-ddl-design/bikes.sql
+++ sql-ddl-design/sql-ddl-design/bikes.sql
@@ -3,3 +3,23 @@ CREATE TABLE customers (
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL
 );
+
+CREATE TABLE models (
+    code VARCHAR(5) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    base_cost NUMERIC NOT NULL
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES customers,
+    placed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMETAMP
+);
+
+CREATE TABLE order_contents (
+    id SERIAL PRIMARY KEY
+    order_id INTEGER REFERENCES orders,
+    model TEXT NOT NULL REFERENCES models,
+    purchase_price NUMERIC NOT NULL,
+    note TEXT
+);
